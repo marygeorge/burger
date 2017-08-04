4ed9e3555123a0574b40c85c3e1b3ac55dcbb9c8
Mary George <reahgeorge@yahoo.com>
2017-08-04 12:10:19 -0400

    connection update

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,11 @@
 var mysql = require("mysql");
-
+var connection;
+if(process.env.JAWSDB_URL)
+{
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+}
+else
+{
 var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
@@ -7,7 +13,7 @@ var connection = mysql.createConnection({
   password: "root",
   database: "burgers_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
